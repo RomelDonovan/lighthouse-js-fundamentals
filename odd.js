1d62d35b927e60be56952d6a627cc4d8b3ea6fb6
RomelDonovan <RomelDonovan@live.ca>
2023-07-26 13:06:29 +0000

    comment console.log output

--- odd.js
+++ odd.js
@@ -2,5 +2,5 @@ const isOdd = function(num) {
   return num % 3 === 0;
 }
 
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
+console.log("3 is odd: " + isOdd(3)); //3 is odd: true
+console.log("8 is odd: " + isOdd(8)); //8 is odd: false
