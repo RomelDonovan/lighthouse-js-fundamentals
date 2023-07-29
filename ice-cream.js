6cdd92f2b0e0a3d442e4f47382473acf59eb6db9
RomelDonovan <RomelDonovan@live.ca>
2023-07-29 19:17:48 +0000

    previous assignments and "packing-list" to iterate over an array with for loop

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//adding "root beer" to the array
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//Someone wants the first flavour of ice cream
+console.log(iceCreamFlavours[0]);
+
+//Someone wants the last flavour of ice cream
+console.log(iceCreamFlavours[5]);
+
+//Someone want to know how many options there are
+console.log(iceCreamFlavours.length);
