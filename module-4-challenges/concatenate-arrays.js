c244c653c4014933c237fe5e92c8996ba7e639cd
RomelDonovan <RomelDonovan@live.ca>
2023-07-31 20:40:49 +0000

    Use function and for loop to concatenate arrays

---
+++ module-4-challenges/concatenate-arrays.js
@@ -0,0 +1,11 @@
+const concat = function(first,second) {
+  for(let x = 0; x < second.length; x++) {
+    first.push(second[x]);
+  }
+  return first;
+};
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
